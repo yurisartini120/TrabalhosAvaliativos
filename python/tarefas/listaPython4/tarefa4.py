7c592b3fcb9823830cb37a34f78e34d5eb14cb64
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-09-19 16:31:25 -0300

    att 4 e 5

---
+++ python/tarefas/listaPython4/tarefa4.py
@@ -0,0 +1,15 @@
+valor1 = int(input("coloque o primeiro valor:"))
+valor2 = int(input("coloque o segundo valor: "))
+auxiliar = 0
+
+lista1 = [(valor1, valor2)]
+
+print("estes são os valores que voce colocou antes", lista1)
+
+auxiliar = valor1
+valor1 = valor2
+
+numeros_trocados = [auxiliar, valor1] 
+print("a nova ordem agora é",numeros_trocados)
+
+
